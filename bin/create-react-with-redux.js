669843d12a083b4e0064a5a13c39b01e86165f60
Johnny Hu <jgeehen.hu@gmail.com>
2017-08-13 20:21:08 -0700

    Setup initial command line interface logic

--- bin/create-react-with-redux.js
+++ bin/create-react-with-redux.js
@@ -1,1 +1,14 @@
 #!/usr/bin/env node
+var program     = require('commander');
+var shell       = require('shelljs');
+var semver      = require('semver');
+var packageJson = require('../package.json');
+
+program
+  .version(semver(packageJson.version))
+  .arguments('<dir>')
+  .usage('[options] <dir>')
+  .action(function(dir) {
+    // logic goes here
+  })
+  .parse(process.argv);
